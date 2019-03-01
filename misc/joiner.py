8ced048b3df05722ce62c66c231202b7d218ac6e
Harry <hs3061@columbia.edu>
2019-03-01 12:24:51 -0500

    added logos and positions for all teams in DB

---
+++ misc/joiner.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+shawn = pd.read_csv("avg_elos_with_pos_color.csv")
+# print(sorted(shawn.team_id.unique()))
+mine = pd.read_csv("nba_colors.csv")
+# print(sorted(mine.team_id.unique()))
+
+
+joined = shawn.set_index("team_id").join(mine.set_index("team_id"))
+
+joined.to_csv("avg_elos_with_pos_color.csv")
