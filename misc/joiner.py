0fabfa640db653a4c6a47e9b3b6d13f939206e60
Harry <hs3061@columbia.edu>
2019-03-24 23:39:10 -0400

    laclips

--- misc/joiner.py
+++ misc/joiner.py
@@ -1,11 +1,19 @@
 import pandas as pd
 
-shawn = pd.read_csv("shawn.csv")
-# print(sorted(shawn.team_id.unique()))
-mine = pd.read_csv("nba_colors.csv")
-# print(sorted(mine.team_id.unique()))
+# shawn = pd.read_csv("shawn.csv")
+# # print(sorted(shawn.team_id.unique()))
+# mine = pd.read_csv("nba_colors.csv")
+# # print(sorted(mine.team_id.unique()))
 
 
-joined = shawn.set_index("team_id").join(mine.set_index("team_id"))
+# joined = shawn.set_index("team_id").join(mine.set_index("team_id"))
 
-joined.to_csv("avg_elos_with_pos_color.csv")
+# joined.to_csv("avg_elos_with_pos_color.csv")
+
+newer = pd.read_csv("avg_elos_with_pos_color.csv")
+
+def update_vals(row):
+    if row.team_id == "LAC":
+        row.lat = 32.715738
+        row.lon = -117.1610838
+    return row
